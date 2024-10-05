bab8f79bde73ecc6a15607ea6912f4474cdfe49a
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-10-04 20:57:51 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS/src/main/java/com/mycompany/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_ciclos;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA2_1_CICLOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    //NOSOTROS ESTABLECEMOS EL PUNTO DE PARTIDA Y EL FINAL   
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + " - ");
+            
+        }
+        System.out.println("");
+        for(int i =1; i <= 10; i++){
+            System.out.println(i + " - ");
+        }
+    }
+}
