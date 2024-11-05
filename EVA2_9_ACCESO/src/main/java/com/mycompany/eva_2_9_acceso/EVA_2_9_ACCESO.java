4df15b91578b9f5a408396d5248bb450336c7321
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-11-05 12:10:52 -0600

    Add files via upload

---
+++ EVA2_9_ACCESO/src/main/java/com/mycompany/eva_2_9_acceso/EVA_2_9_ACCESO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva_2_9_acceso;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA_2_9_ACCESO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
