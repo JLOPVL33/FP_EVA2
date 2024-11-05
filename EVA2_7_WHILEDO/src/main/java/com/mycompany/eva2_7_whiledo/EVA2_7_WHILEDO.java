7e1358db6be9c48183a5ef15d1107ac320cb4224
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-11-05 12:09:53 -0600

    Add files via upload

---
+++ EVA2_7_WHILEDO/src/main/java/com/mycompany/eva2_7_whiledo/EVA2_7_WHILEDO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_whiledo;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA2_7_WHILEDO {
+
+    public static void main(String[] args) {
+        System.out.println("WHILE");
+        int val=1;
+        while(val<=10){
+            System.out.println(val+" - ");
+           val++;
+        }
+        val=1;
+          System.out.println("DO WHILE");
+    do{
+         
+       System.out.println(val+" - ");
+           val++;  
+    }while(val<=10);
+    }
+}
