d51615ddf25a345802844f191bbeb5d5d39fd7db
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-11-05 12:28:36 -0600

    Add files via upload

---
+++ EVA2_14_BINARIOS/src/main/java/com/mycompany/eva2_14_binarios/EVA2_14_BINARIOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_14_binarios;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA2_14_BINARIOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
