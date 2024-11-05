2438d1f73dd120991040f8ae3bdad363ed8e2d80
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-11-05 12:27:48 -0600

    Add files via upload

---
+++ EVA2_12_NUMERO_MAYOR/src/main/java/com/mycompany/eva2_12_numero_mayor/EVA2_12_NUMERO_MAYOR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_12_numero_mayor;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA2_12_NUMERO_MAYOR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
