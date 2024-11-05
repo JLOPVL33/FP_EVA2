970aef65e69134d903563a080f1429aa6a037945
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-11-05 12:45:46 -0600

    Add files via upload

---
+++ EVA2_15_ARREGLOS/src/main/java/com/mycompany/eva2_15_arreglos/EVA2_15_ARREGLOS.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_arreglos;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA2_15_ARREGLOS {
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[0] = 100;
+        
+        arr[1] = 200;
+        
+        arr[2] = 300;
+        
+        arr[3] = 400;
+        
+        arr[4] = 500;
+        
+        arr[5] = 600;
+        
+        arr[6] = 700;
+        
+        arr[7] = 800;
+        
+        arr[8] = 900;
+        
+        arr[9] = 1000;
+        
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
+}
