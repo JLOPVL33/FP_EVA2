d24f15d40e0de0cae169601d6c53eea8bb0a420c
JLOPVL33 <L24550321@chihuahua2.tecnm.mx>
2024-11-05 12:08:45 -0600

    Add files via upload

---
+++ EVA2_5_BREAK/src/main/java/com/mycompany/eva2_5_break/EVA2_5_BREAK.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_5_break;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class EVA2_5_BREAK {
+
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+           if(i==8)
+               break;
+           System.out.println(i+" - "); 
+    }
+}}
